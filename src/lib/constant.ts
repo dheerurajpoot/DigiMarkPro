dfbce27fb2375be466e9a30451ec95ae942259a7
Dheeru Rajpoot <150139748+dheerurajpoot@users.noreply.github.com>
2025-03-18 12:31:41 +0530

    Update constant.ts

--- src/lib/constant.ts
+++ src/lib/constant.ts
@@ -1,4 +1,4 @@
-export const DOMAIN_NAME = "https://digimarkpro.com";
+export const DOMAIN_NAME = "https://digimarkpro.vercel.app";
 export const COMPANY_NAME = "DigiMarkPro";
 export const COMPANY_EMAIL = "contact@digimarkpro.com";
 export const COMPANY_PHONE = "+917755089819";
