424fc3617056964929a17d5758a1377af35cea20
Dheeru Rajpoot <150139748+dheerurajpoot@users.noreply.github.com>
2025-04-14 16:14:33 +0530

    Update constant.ts

--- src/lib/constant.ts
+++ src/lib/constant.ts
@@ -1,5 +1,5 @@
-export const DOMAIN_NAME = "https://digimarkpro.vercel.app";
-export const COMPANY_NAME = "DigiMarkPro";
-export const COMPANY_EMAIL = "contact@digimarkpro.com";
+export const DOMAIN_NAME = "https://www.orbtao.com";
+export const COMPANY_NAME = "Orbtao";
+export const COMPANY_EMAIL = "drexpress90@gmail.com";
 export const COMPANY_PHONE = "+917755089819";
 export const COMPANY_ADDRESS = "Kalyanpur Kanpur Nagar, UP, India (208017)";
